bee6cd0af0f638692782a061f8583827afb4f506
emoto-yasushi <106805183+emoto-yasushi@users.noreply.github.com>
2022-12-22 22:32:00 +0900

    Create osc_server_vmc.py

---
+++ osc_server_vmc.py
@@ -0,0 +1,24 @@
+import argparse
+import threading
+import argparse
+import pythonosc
+from pythonosc import osc_server
+from pythonosc import dispatcher
+
+def receive_VMC_OSC_BlendShape(unused_addr,BlendShapeName_args,Value):
+	print("BlendShapeName:" + BlendShapeName_args + "\n")
+	#print("BlendShapeValue:" + str(Value) + "\n")
+
+def receive_VMC_OSC_Pos(unused_addr,BoneName,Value0,Value1,Value2,Value3,Value4,Value5,Value6):
+	print("BoneName:"+BoneName + "\n")
+	#print("BoneValues:" + str(Value0) + "," + str(Value1) + "," + str(Value2) + "," + str(Value3) + "," + str(Value4) + "," + str(Value5) + "," + str(Value6) + "\n")
+	
+if __name__ == "__main__":
+	vmc_dispatcher = dispatcher.Dispatcher()
+	vmc_dispatcher.map("/VMC/Ext/Blend/Val", receive_VMC_OSC_BlendShape)
+	vmc_dispatcher.map("/VMC/Ext/Bone/Pos", receive_VMC_OSC_Pos)
+
+	vmc_recieve_server = osc_server.ThreadingOSCUDPServer(("192.168.128.164", 39540), vmc_dispatcher)
+
+	vmc_recieve_thread = threading.Thread(target=vmc_recieve_server.serve_forever)
+	vmc_recieve_thread.start()
